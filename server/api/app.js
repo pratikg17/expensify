5a4265363ba7f6b07fcbacf1d61377c9ea5561a6
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-11 18:02:31 -0400

    Added midddle ware

--- server/api/app.js
+++ server/api/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import routes from "./routes/index.js";
+import "dotenv/config";
 
 const app = express();
 
 // Mongo db connection
-mongoose.connect("mongodb://localhost:27017/expensedb");
+// console.log("process.env", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(express.urlencoded());
