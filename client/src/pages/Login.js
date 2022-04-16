addc98fd7a81ed4a9bb3425b1a50d87906dfb7b6
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-15 22:58:34 -0400

    Added Form validation for Registration

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -121,7 +121,7 @@ export default function Login() {
               </Button>
               <Grid container>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link to="/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,9 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -13,18 +10,32 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Copyright from "../components/Copyright";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 
 const theme = createTheme();
 
 export default function Register() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
     console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+      email: data.email,
+      name: data.name,
+      password: data.password,
     });
   };
+
+  const handleSubmit1 = (event) => {};
 
   return (
     <ThemeProvider theme={theme}>
@@ -44,72 +55,84 @@ export default function Register() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box
+          <form
             component="form"
-            noValidate
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             sx={{ mt: 3 }}
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
+            <Box>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={12}>
+                  {/* <TextField
+                    autoComplete="given-name"
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Full Name"
+                    autoFocus
+                  /> */}
+
+                  <TextField
+                    id="outlined-basic"
+                    name="name"
+                    label="Full Name"
+                    variant="outlined"
+                    fullWidth
+                    {...register("name", {
+                      required: "Full Name is required.",
+                    })}
+                    error={Boolean(errors.name)}
+                    helperText={errors.name?.message}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="email"
+                    label="E-mail"
+                    variant="outlined"
+                    fullWidth
+                    name="email"
+                    {...register("email", {
+                      required: "E-mail Address is required.",
+                    })}
+                    error={Boolean(errors.email)}
+                    helperText={errors.email?.message}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    type="password"
+                    name="password"
+                    {...register("password", {
+                      required: "Passowrd is required.",
+                    })}
+                    error={Boolean(errors.password)}
+                    helperText={errors.password?.message}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link to="/login" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+            </Box>
+          </form>
         </Box>
         <Copyright sx={{ mt: 5 }} />
       </Container>
