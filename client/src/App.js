f81bfa9f3d7963772980288d78f8342ff29ff85d
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-15 20:18:44 -0400

    Base framework for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import "antd/dist/antd.css";
 import "./App.scss";
 
 import { Route, BrowserRouter, Redirect } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/User/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/" exact component={Login}></Route>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/login" exact component={Login}></Route>
         <Route path="/register" exact component={Register}></Route>
       </BrowserRouter>
     </div>
---
+++ client/src/pages/User/listItems.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Reports" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
+export const secondaryListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <LogoutIcon />
+      </ListItemIcon>
+      <ListItemText primary="Logout" />
+    </ListItemButton>
+  </React.Fragment>
+);
