7d7dc49f2b92dbac18f3926652f9261b0c7f267f
Shenckerr Gollapudi <gollapudi.sh@northeastern.edu>
2022-04-23 16:32:37 -0400

    category wise report UI

--- client/src/pages/User/Reports/CategoryWiseReport.js
+++ client/src/pages/User/Reports/CategoryWiseReport.js
@@ -70,5 +70,102 @@ function CategoryWiseReport() {
         };
         dispatch(getCategoryWiseReport(query));
       };
-    
+   
+      return (
+        <DefaultLayout>
+          {loading && <Spinner />}
+          <Container maxWidth="100%">
+            <Typography variant="h6" gutterBottom>
+              Category-wise expenditures
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-around",
+                p: 1,
+                m: 1,
+                bgcolor: "background.paper",
+                borderRadius: 1,
+              }}
+            >
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="From..."
+                  showTodayButton
+                  value={firstDay}
+                  disableFuture
+                  onChange={handleDateChange("firstDay")}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="To..."
+                  showTodayButton
+                  value={lastDay}
+                  onChange={handleDateChange("lastDay")}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+              <Box sx={{ p: 1, m: 2, justifyContent: "space-between" }}>
+                <Button variant="contained" color="success" onClick={searchClicked}>
+                  FILTER
+                </Button>
+                &nbsp;
+                <Button variant="contained" color="error" onClick={resetClicked}>
+                  RESET
+                </Button>
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                p: 1,
+                m: 1,
+                bgcolor: "background.paper",
+                borderRadius: 1,
+              }}
+              component={Paper}
+            >
+              <div style={{ width: 550, margin: "auto" }}>
+                <svg viewBox="0 0 320 320">
+                  <VictoryPie
+                    standalone={false}
+                    data={expenses.monthAVG}
+                    innerRadius={50}
+                    theme={VictoryTheme.material}
+                    labelRadius={({ innerRadius }) => innerRadius + 14}
+                    labelComponent={
+                      <VictoryLabel
+                        angle={0}
+                        style={[
+                          {
+                            fontSize: "11px",
+                            fill: "#0f0f0f",
+                          },
+                          {
+                            fontSize: "10px",
+                            fill: "#013157",
+                          },
+                        ]}
+                        text={({ datum }) => `${datum.x}\n $${datum.y}`}
+                      />
+                    }
+                  />
+                  <VictoryLabel
+                    textAnchor="middle"
+                    style={{ fontSize: 12, fill: "#8b8b8b" }}
+                    x={175}
+                    y={170}
+                    text={`Spent \nper category`}
+                  />
+                </svg>
+              </div>
+            </Box>
+          </Container>
+        </DefaultLayout>
+      );
+    }  
+      
 export default CategoryWiseReport;
