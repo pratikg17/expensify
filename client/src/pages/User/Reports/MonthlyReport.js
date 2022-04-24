7b725345c76ca6332c68567d12aa8c8a4f5e8639
Shenckerr Gollapudi <gollapudi.sh@northeastern.edu>
2022-04-24 15:44:44 -0400

    monthwise reports

--- client/src/pages/User/Reports/MonthlyReport.js
+++ client/src/pages/User/Reports/MonthlyReport.js
@@ -40,6 +40,89 @@ function MonthlyReport() {
         let data = { month: date.toDateString() };
         dispatch(getMonthlyReport(data));
       };
+      return (
+        <DefaultLayout>
+          {loading && <Spinner />}
+          <Container maxWidth="100%">
+            <Typography variant="h6" gutterBottom>
+              Monthly Scatter Plot
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                p: 1,
+                m: 1,
+                bgcolor: "background.paper",
+                borderRadius: 1,
+              }}
+            >
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Select Month"
+                  views={["month"]}
+                  showTodayButton
+                  value={month}
+                  disableFuture
+                  animateYearScrolling
+                  onChange={(newValue) => {
+                    handleDateChange(newValue);
+                  }}
+                  variant="inline"
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                p: 1,
+                m: 1,
+                bgcolor: "background.paper",
+                borderRadius: 1,
+              }}
+              component={Paper}
+            >
+              <VictoryChart
+                theme={VictoryTheme.material}
+                height={400}
+                width={550}
+                domainPadding={40}
+              >
+                <VictoryScatter
+                  style={{
+                    data: { fill: "#01579b", stroke: "#69f0ae", strokeWidth: 2 },
+                    labels: { fill: "#01579b", fontSize: 10, padding: 8 },
+                  }}
+                  bubbleProperty="y"
+                  maxBubbleSize={15}
+                  minBubbleSize={5}
+                  labels={({ datum }) => `$${datum.y} on ${datum.x}th`}
+                  labelComponent={<VictoryTooltip />}
+                  data={plot}
+                  domain={{ x: [0, 31] }}
+                />
+                <VictoryLabel
+                  textAnchor="middle"
+                  style={{ fontSize: 14, fill: "#8b8b8b" }}
+                  x={270}
+                  y={390}
+                  text={`day of month`}
+                />
+                <VictoryLabel
+                  textAnchor="middle"
+                  style={{ fontSize: 14, fill: "#8b8b8b" }}
+                  x={6}
+                  y={190}
+                  angle={270}
+                  text={`Amount ($)`}
+                />
+              </VictoryChart>
+            </Box>
+          </Container>
+        </DefaultLayout>
+      );      
 
     }      
 export default MonthlyReport;
