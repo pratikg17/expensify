573d7021a3ad08f2d776be389a80b4cb57f508b2
SathwikHegde <hegde.sat@northeastern.edu>
2022-04-25 06:09:07 -0400

    Setting up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+
 import { Route, BrowserRouter, Redirect } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,16 +9,23 @@ import "react-toastify/dist/ReactToastify.css";
 import AddExpense from "./pages/User/AddExpense";
 import ListExpense from "./pages/User/ListExpense";
 import UpdateExpense from "./pages/User/UpdateExpense";
+import MonthlyReport from "./pages/User/Reports/MonthlyReport";
+import CategoryWiseReport from "./pages/User/Reports/CategoryWiseReport";
+import YearlyReport from "./pages/User/Reports/YearlyReport";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <ProtectedRoute path="/" exact component={Home}></ProtectedRoute>
         <ProtectedRoute
           path="/add-expense"
           exact
           component={AddExpense}
+        ></ProtectedRoute>
+        <ProtectedRoute
+          path="/update-expense/:expenseid"
+          exact
+          component={UpdateExpense}
         ></ProtectedRoute>
         <ProtectedRoute
           path="/list-expense"
@@ -25,10 +33,21 @@ function App() {
           component={ListExpense}
         ></ProtectedRoute>
         <ProtectedRoute
-          path="/update-expense/:expenseid"
+          path="/monthly"
           exact
-          component={UpdateExpense}
+          component={MonthlyReport}
         ></ProtectedRoute>
+        <ProtectedRoute
+          path="/yearly"
+          exact
+          component={YearlyReport}
+        ></ProtectedRoute>
+        <ProtectedRoute
+          path="/category-wise"
+          exact
+          component={CategoryWiseReport}
+        ></ProtectedRoute>
+        <ProtectedRoute path="/" exact component={Home}></ProtectedRoute>
         <Route path="/login" exact component={Login}></Route>
         <Route path="/register" exact component={Register}></Route>
       </BrowserRouter>
