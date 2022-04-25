6a5f322d6be1bfaa7ba57d2d63b19707fd46b00c
Pratik Gawand <gawand.p@northeastern.edu>
2022-04-25 13:38:28 -0400

    Baseurl for frontend

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -2,7 +2,15 @@ const urls = new Map();
 const websiteUrls = new Map();
 
 urls.set("localhost", "http://localhost:9000");
+urls.set(
+  "expensify-client-web-app.herokuapp.com",
+  "https://expensify-client-web-app.herokuapp.com:9000"
+);
 websiteUrls.set("localhost", "http://localhost:3000");
+websiteUrls.set(
+  "expensify-client-web-app.herokuapp.com",
+  "https://expensify-client-web-app.herokuapp.com"
+);
 
 export const baseUrl = urls.get(window.location.hostname);
 export const websiteUrl = websiteUrls.get(window.location.hostname);
